6a508a69e979c1ec439bffe08b289e6a2948091d
Usernameman <32287468+Usernameman@users.noreply.github.com>
2022-05-26 10:34:21 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+# Define the function "on_forever" This sets out what the function does.
+def on_forever():
+    basic.show_number(199)
+
+# Call the Function to do its job
+basic.forever(on_forever)
+# Define the function "on_forever" This sets out what the function does.
+
---
+++ main.ts
@@ -0,0 +1,5 @@
+//  Define the function "on_forever" This sets out what the function does.
+//  Call the Function to do its job
+basic.forever(function on_forever() {
+    basic.showNumber(199)
+})
